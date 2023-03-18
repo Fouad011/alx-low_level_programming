02e8b65227db041436e292a85c79fcbcfe2f91e9
Fouad011 <mourchidfouad22@hotmail.com>
2023-03-18 08:04:21 -0700

    print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,27 @@
+#include <stdio.h>
 /**
-* main - prints all combination of single digits
+* main - prints all combination3 of single digits
 * Return: Always 0
 */
 int main(void)
 {
-	int n;
-
-	for (n = 1; n < 90; n++)
+	int p = 0;
+	int n = 1;
+	int q = 1;
+	for (p = 0; p < 9; p++)
 	{
-		if (n < 10)
+		for (; q < 10; q++)
 		{
-			putchar('0' + n);
+			putchar('0' + p);
+			putchar('0' + q);
+			if (p != 8 || q != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
-		else
-		{
-			putchar(n);
-		}
-		if (n != 89)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		q = ++n;
 	}
-
 	putchar('\n');
-
 	return (0);
 }
