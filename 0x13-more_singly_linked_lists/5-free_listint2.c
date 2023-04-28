54c45766445c81e6d088be3a6fe37f9e52fc7018
Fouad011 <125674411+Fouad011@users.noreply.github.com>
2023-04-28 10:48:33 +0000

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,7 +2,7 @@
 
 /**
  * free_listint2 - frees a linked list
- * @head: pointer to the listint_t list to be freed
+ * head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
 {
@@ -20,4 +20,3 @@ void free_listint2(listint_t **head)
 
 	*head = NULL;
 }
-
